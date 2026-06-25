822a758b57e5f1dc6a2de84649150f2a40ffb673
Anoop Kurungadam <anoop@earthianslive.com>
2026-06-25 12:17:47 +0530

    fix: payments app version
    (cherry picked from commit e5b41205c5939fa140a679bec27e9fd6eec6692e)
    
    # Conflicts:
    #	.github/helper/install.sh

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -40,8 +40,13 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
+<<<<<<< HEAD
 bench get-app payments
 bench get-app https://github.com/frappe/erpnext --branch "$BRANCH_TO_CLONE"
+=======
+bench get-app payments --branch "${branchtoclone}"
+bench get-app https://github.com/frappe/erpnext --branch "${branchtoclone}"
+>>>>>>> e5b4120 (fix: payments app version)
 bench get-app healthcare "${GITHUB_WORKSPACE}"
 
 bench setup requirements --dev
